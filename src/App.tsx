b77edd5031f879320fa2e8e11dd08359ef1e28d3
Timothy Cool <tcoolpg@gmail.com>
2024-01-14 21:12:16 -0800

    fix: add the divs for each example back in

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ const App: Component = () => {
 	return (
 		<div class={styles.App}>
 			<Section />
+			<div class={styles.showcase}>
+				<div class="game-demo" id="pong"></div>
+				<div class="game-demo" id="breakout"></div>
+				<div class="game-demo" id="space-invaders"></div>
+				<div class="game-demo" id="asteroids"></div>
+				<div class="game-demo" id="platformer"></div>
+			</div>
 		</div>
 	);
 };
